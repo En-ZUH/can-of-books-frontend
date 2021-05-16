e4dde4ee2d1abe24b8502240bf1eca5a93abff3f
Ibrahim Abu-awad <ibrahimabuawadwork@gmail.com>
2021-05-16 17:51:08 +0300

    Done

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,29 +1,19 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
-import { useAuth0 } from '@auth0/auth0-react';
 
-function MyFavoriteBooks() {
-  const {
-    isAuthenticated,
-    loginWithRedirect,
-  } = useAuth0();
-
-
-  return isAuthenticated && (
-    <>
+class MyFavoriteBooks extends React.Component {
+  render() {
+    return(
       <Jumbotron>
         <h1>My Favorite Books</h1>
         <p>
           This is a collection of my favorite books
         </p>
       </Jumbotron>
-
-    </>
-  );
-
+    )
+  }
 }
 
 export default MyFavoriteBooks;
