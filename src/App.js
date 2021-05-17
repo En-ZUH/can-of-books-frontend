b1a9a4ea336e5ca81b9d97e454ca588879b90a2c
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-05-17 23:15:09 +0300

    done

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
 
   render() {
     console.log('app', this.props);
+    //const {user, is Authenticated}=this.props.auth0;
     return (
       <>
         <Router>
